d61e18a208f7b694a064c8514110e38be712f182
Talon Hughes <talon.hughes@gmail.com>
2017-04-28 13:34:08 -0500

    Added foundation classes to improve layout

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to the Examples page</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few examples locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Provo'>Provo, Utah</Link>
+        </li>
+        <li>
+          <Link to='/?location=Puebla'>Puebla, Mexico</Link>
+        </li>
+      </ol>
     </div>
   )
 };
