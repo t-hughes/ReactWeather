9477984a966f26b4c50834a5c14a5d1088190d44
Talon Hughes <talon.hughes@gmail.com>
2017-04-28 19:00:51 -0500

    Added foundation to tools list

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -16,6 +16,9 @@ var About = (props) => {
           <li>
             <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
           </li>
+          <li>
+            <a href="https://foundation.zurb.com">Foundation</a> - This is the CSS framework I used to make things look pretty.
+          </li>
         </ul>
       </p>
     </div>
