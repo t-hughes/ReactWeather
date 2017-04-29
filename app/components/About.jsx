789f2f6f530e136ffbd660d9f786095c133bfe87
Talon Hughes <talon.hughes@gmail.com>
2017-04-28 19:04:08 -0500

    Added target attribute to links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,13 +11,13 @@ var About = (props) => {
         Here are some of the tools I used:
         <ul>
           <li>
-            <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+            <a href="https://facebook.github.io/react" target="_blank">React</a> - This was the JavaScript framework used.
           </li>
           <li>
-            <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+            <a href="http://openweathermap.org" target="_blank">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
           </li>
           <li>
-            <a href="https://foundation.zurb.com">Foundation</a> - This is the CSS framework I used to make things look pretty.
+            <a href="https://foundation.zurb.com" target="_blank">Foundation</a> - This is the CSS framework I used to make things look pretty.
           </li>
         </ul>
       </p>
